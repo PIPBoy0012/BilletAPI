226b61861b8c353dd8ccdf3f33ebe30e3f79b1f3
Thias schou <thiasbabs@gmail.com>
2022-01-05 14:58:18 +0100

    Add project files.

---
+++ BilletAPI/Controllers/Koebscontroller.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BilletAPI.Controllers
+{
+    public class Koebscontroller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //public IActionResult Get()
+        //{
+        //    return new Billet;
+        //}
+
+        public IActionResult Post(int userID, int eventID)
+        {
+            return Ok("Yes");
+        }
+    }
+}
---
+++ BilletAPI/ObjectModels/Billet.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BilletAPI.ObjectModels
+{
+    public class Billet
+    {
+        int id;
+        DateTime dato;
+        string eventting;
+
+        public Billet(int iD, DateTime datO, string evenT)
+        {
+            id = iD;
+            dato = datO;
+            eventting = evenT;
+        }
+    }
+}
